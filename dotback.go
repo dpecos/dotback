6c8371cd033933ba2c568ad82fd6af40cbaaaa4b
Daniel Pecos Martinez <contact@danielpecos.com>
2017-01-16 18:37:27 +0100

    Quick dirty hack to list recipes

--- dotback.go
+++ dotback.go
@@ -13,7 +13,7 @@ import (
 	. "github.com/dpecos/dotback/utils"
 )
 
-func ReadConfig() []models.Recipe {
+func ReadConfig(recipe string) []models.Recipe {
 	file, err := ioutil.ReadFile(path.Join(HomeDir(), ".dotfiles", "config.json"))
 	CheckError("Could not read config.json file", err)
 
@@ -21,7 +21,14 @@ func ReadConfig() []models.Recipe {
 	err = json.Unmarshal(file, &config)
 	CheckError("Could not parse config.json file", err)
 
-	// fmt.Printf("%+v", config)
+	if recipe != "" {
+		for _, r := range config {
+			if r.Name == recipe {
+				fmt.Printf("Executing only recipe '%s' (skipping the rest)\n", r.Name)
+				config = []models.Recipe{r}
+			}
+		}
+	}
 
 	return config
 }
@@ -35,10 +42,11 @@ func ExecRecipes(config []models.Recipe) {
 var (
 	app = kingpin.New("dotback", "Handle your dot files like a boss")
 	// initialize = app.Command("init", "Use a Git repository to initialize your dotfiles folder")
-	pull = app.Command("pull", "Fetch latest changes from remote dotfiles repository")
-	push = app.Command("push", "Send latest changes to remote dotfiles repository")
-	// install    = app.Command("install", "Performs the actions defined in your ~/.dotfiles/config.json")
-	// recipe     = install.Arg("recipe", "Execute only this recipe").String()
+	pull    = app.Command("pull", "Fetch latest changes from remote dotfiles repository")
+	push    = app.Command("push", "Send latest changes to remote dotfiles repository")
+	list    = app.Command("list", "List the actions defined in your ~/.dotfiles/config.json")
+	install = app.Command("install", "Performs the actions defined in your ~/.dotfiles/config.json")
+	recipe  = install.Arg("recipe", "Execute only this recipe").String()
 	// add        = app.Command("add", "Creates a new recipe")
 	// delete     = app.Command("delete", "Remove a recipe")
 )
@@ -54,21 +62,26 @@ func main() {
 		path := path.Join(HomeDir(), ".dotfiles")
 		err := Execute(fmt.Sprintf("cd %s && git push", path))
 		CheckError("Error updating repository", err)
+	case "list":
+		config := ReadConfig(*recipe)
+		for _, recipe := range config {
+			fmt.Printf("%s -> \n", recipe.Name)
+
+			for _, action := range recipe.Actions {
+				if action.Link != "" {
+					fmt.Printf("   Link: %s\n", action.Link)
+				}
+				if action.Cmd != "" {
+					fmt.Printf("   Cmd: %s\n", action.Cmd)
+				}
+				if action.Git != "" {
+					fmt.Printf("   Git: %s\n", action.Git)
+				}
+			}
+		}
+	case "install":
+		config := ReadConfig(*recipe)
+		ExecRecipes(config)
 	}
 
-	// config := ReadConfig()
-
-	// if *recipe != "" {
-	// 	for _, r := range config {
-	// 		if r.Name == *recipe {
-	// 			fmt.Printf("Executing only recipe '%s' (skipping the rest)\n", r.Name)
-	// 			config = []models.Recipe{r}
-	// 		}
-	// 	}
-	// }
-
-	// fmt.Println(config)
-	// if *action {
-	// 	//ExecRecipes(config)
-	// }
 }
